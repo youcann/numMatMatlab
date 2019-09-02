486a342bcd98e608cf5949ae2108e37133cd5b56
soerman <kescha356@gmail.com>
2019-09-02 14:59:05 +0200

    little experiment with lr

--- lr.m
+++ lr.m
@@ -2,8 +2,18 @@ clc;
 clear all;
 close all;
 
-A= [2 1 1; 
-    1 9 2; 
-    1 2 1];
+A= [1 2 3; 
+    2 -4 6; 
+    3 -9 -3];
 
+%P ist in L integriert
 [L R]=lu(A)
+
+
+%L wird normal ausgegeben
+[L2 R P]=lu(A)
+
+
+P*L
+
+
